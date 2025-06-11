b5efe466e8d4c6cc3692121832546e84f0a2127b
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-06-11 17:50:18 +0600

    store setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { use } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from './redux/features/counterSlice';
 
 function App() {
-
-
+const {count}=useSelector((state)=> state.counter);
+const dispatch=useDispatch();
   return (
     <>
-    <h1 className='text-5xl uppercase'>react with redux.</h1>
+      <div>
+        <button onClick={()=>dispatch(increment())} className='px-3 cursor-pointer'>Increment</button>
+        <h2>{ count}</h2>
+        <button onClick={()=>dispatch(decrement())}>Decrement</button>
+   </div>
     </>
   )
 }
